db02ea3bc74d0cc921fdaf1865a5158560382289
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-10-09 19:32:16 +0900

    quick sort로 수정(시간초과)

--- Python/10867.py
+++ Python/10867.py
@@ -1,39 +1,35 @@
 N = int(input())
-unsorted = input().split()
-unsorted = list(map(int, unsorted))
-ctr = N-1
-idx = 0
+unsorted_list = input().split()
+unsorted_list = list(map(int, unsorted_list))
 
-#버블소트 도중 중복된 쌍이 있으면 제거한다. 버블소트를 써서 느린듯..
-while True:
-    #탐색횟수를 모두 소모하면 종료한다.
-    if ctr == 0:
-        break
 
-    num1 = unsorted[idx]
-    num2 = unsorted[idx + 1]
+def quick_sort(prm_list):
+    length = len(prm_list)
+    if length < 2:
+        return prm_list
+    lesser = []
+    greater = []
 
-    # 오름차순으로 스위치
-    if num1 > num2:
-        num1 = num1 ^ num2
-        num2 = num1 ^ num2
-        num1 = num1 ^ num2
-        unsorted[idx] = num1
-        unsorted[idx + 1] = num2
+    pivot = prm_list.pop(0)
+
+    idx = 0
+    for _ in prm_list[:]:
+
+        if prm_list[idx] < pivot:
+            lesser.append(prm_list.pop(idx))
+        elif prm_list[idx] > pivot:
+            greater.append(prm_list.pop(idx))
+        else:
+            prm_list.pop(idx)
         idx += 1
 
-    #같은 숫자가 나오면 제거하고, 전체길이와 탐색 횟수를 줄인다.
-    elif num1 == num2:
-        unsorted.pop(idx)
-        N -= 1
-        ctr -= 1
-    # 오름차순이면 이미 넘어간다.
-    elif num1 < num2:
-        idx += 1
+    lesser = quick_sort(lesser)
+    greater = quick_sort(greater)
 
-    # 남은 리스트의 끝까지 탐색한경우 idx를 처음으로 돌린다. 탐색횟수를 소모한다.
-    if idx == N-1:
-        idx = 0
-        ctr -= 1
+    lesser.append(pivot)
+    ans = lesser + greater
 
-print(unsorted)
+    return ans
+
+
+print(quick_sort(unsorted_list))

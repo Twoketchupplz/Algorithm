63cbe46f060c541efa23785cfe41205fc14d012d
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-10-09 15:47:33 +0900

    while문 하나로 짰는데 시간초과..

--- Python/10867.py
+++ Python/10867.py
@@ -2,29 +2,33 @@ N = int(input())
 unsorted = input().split()
 unsorted = list(map(int, unsorted))
 ans = []
-ctr = 0
+ctr = N-1
+idx = 0
 
 while True:
-    idx = 0
-    if ctr == N:
+    if ctr == 0:
         break
-    while True:
-        if idx == N-1:
-            break
-        num1 = unsorted[idx]
-        num2 = unsorted[idx+1]
-        if num1 > num2:
-            num1 = num1 ^ num2
-            num2 = num1 ^ num2
-            num1 = num1 ^ num2
-            unsorted[idx] = num1
-            unsorted[idx+1] = num2
-            idx += 1
-        elif num1 == num2:
-            unsorted.pop(idx)
-            N -= 1
-        else :
-            idx += 1
-    ctr += 1
+
+    num1 = unsorted[idx]
+    num2 = unsorted[idx + 1]
+
+    if num1 > num2:
+        num1 = num1 ^ num2
+        num2 = num1 ^ num2
+        num1 = num1 ^ num2
+        unsorted[idx] = num1
+        unsorted[idx + 1] = num2
+        idx += 1
+    elif num1 == num2:
+
+        unsorted.pop(idx)
+        N -= 1
+        ctr -= 1
+    elif num1 < num2:
+        idx += 1
+
+    if idx == N-1:
+        idx = 0
+        ctr -= 1
 
 print(unsorted)

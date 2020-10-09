09070e3f7f40e6964e2080f54de6bec9738e10e6
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-10-10 01:38:56 +0900

    중복빼고 정렬하기 39744KB, 348ms, 677B

--- Python/10867.py
+++ Python/10867.py
@@ -1,25 +1,20 @@
-N = int(input())
-unsorted_list = input().split()
-unsorted_list = list(map(int, unsorted_list))
-
-
 def quick_sort(prm_list):
     length = len(prm_list)
     if length < 2:
         return prm_list
+
     lesser = []
     greater = []
 
-    pivot = prm_list.pop(0)
+    pivot = prm_list.pop()
 
-    for i in prm_list[:]:
-
-        if i < pivot:
-            lesser.append(prm_list.pop(0))
-        elif i > pivot:
-            greater.append(prm_list.pop(0))
+    for i in range(length - 2, -1, -1):
+        if prm_list[i] < pivot:
+            lesser.append(prm_list.pop())
+        elif prm_list[i] > pivot:
+            greater.append(prm_list.pop())
         else:
-            prm_list.pop(0)
+            prm_list.pop()
 
     lesser = quick_sort(lesser)
     greater = quick_sort(greater)
@@ -30,4 +25,8 @@ def quick_sort(prm_list):
     return ans
 
 
-print(quick_sort(unsorted_list))
+N = int(input())
+unsorted_list = input().split()
+unsorted_list = list(map(int, unsorted_list))
+
+print(" ".join(map(str, quick_sort(unsorted_list))))

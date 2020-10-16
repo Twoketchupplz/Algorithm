6638b66d6e6feac0b34aac5854ad55c904808db5
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-10-16 23:56:26 +0900

    인구이동 다음 union search를 시작할 때 nation을 정확히 인지

--- Python/16234.py
+++ Python/16234.py
@@ -11,27 +11,25 @@ def bfs(n, left, right):
     y = [-1, 0, 1, 0]
     day = 0
 
-    # 더이상 day 가 늘어나지 않을 때까지 찾는다. 이부분이 미지수 Z
+    # day가 늘어나지 않을때까지 찾는다.
     while True:
         visited = [list(False for _ in range(n)) for _ in range(n)]
         move = False
         next_nation = [0, 0]
-        counter = 0
+        counter, next_i, next_j, temp_i, temp_j = 0, 0, 0, 0, 0
 
-        # 하루동안 인구 변화 O()
+        #더이상 union이 없을때까지 찾는다.
         while True:
             uni_pos = []
             uni_q = [[next_nation[0], next_nation[1]]]
             uni_pos.append([next_nation[0], next_nation[1]])
             num_nation = 1
 
-            # union을 bfs로 찾는다. O(N)
             while uni_q:
                 current = uni_q.pop(0)
                 visited[current[0]][current[1]] = True
                 counter += 1
 
-                # 사방을 조사한다. O(4)
                 for i in range(4):
                     next_row = current[0] + x[i]
                     next_col = current[1] + y[i]
@@ -45,7 +43,7 @@ def bfs(n, left, right):
                         uni_pos.append([next_row, next_col])
                         num_nation += 1
 
-            # union간 인구이동이 발생시 이동시킨다. 이때 이동이 있었음을 감지할 수 있다.
+            # 연합국 인구 분배
             if num_nation > 1:
                 move = True
                 uni_population = 0
@@ -55,11 +53,14 @@ def bfs(n, left, right):
                 for nation in uni_pos:
                     population[nation[0]][nation[1]] = each_population
 
-            # 다음 시작 nation을 찾는다.
-            for j in range(n):
-                for i in range(n):
-                    if not visited[i][j]:
-                        next_nation = [i, j]
+            # 다음 union search를 시작할 nation
+            next_i, next_j = temp_i, temp_j
+            for j in range(next_j, n):
+                temp_j = j
+                for i in range(next_i, n):
+                    next_nation = [i, j]
+                    temp_i = i
+
 
             if counter == n*n:
                 break

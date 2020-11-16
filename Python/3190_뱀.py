2fa81e0e64a5fb995589c20672f023a8c2a14dda
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-11-16 19:58:53 +0900

    뱀 풀이

--- Python/3190_뱀.py
+++ Python/3190_뱀.py
@@ -1,68 +1,64 @@
-# 넘파이는 쓰면 안되는 거시여따..
-import numpy as np
+def Dummy():
+    N = int(input())
+    K = int(input())
 
-def Dummy(N, apple, plan):
-    board = np.zeros((N + 1, N + 1), int)
+    board = [[0]*(N+1) for _ in range(N+1)]
+    board[1][1] = 1
 
+    apple = []
+    for _ in range(K):
+        a_row, a_col = list(map(int, input().split()))
+        apple.append([a_row, a_col])
+    num_rotate = int(input())
     for pos in apple:
-        board[pos[0], pos[1]] = -1
-    board[1, 1] = 1
+        board[pos[0]][pos[1]] = 2
 
-    plan.append([10001, 'L'])
-    plan.reverse()
+    route = []
+    for _ in range(num_rotate):
+        sec, direction = input().split()
+        route.append([int(sec), direction])
+    route.append([10001, 'L'])
 
-    return end_time(board, plan)
+    return end_time(N, board, route)
 
 
-def end_time(board, route_list):
+def end_time(n, board, route_list):
     ans = 0
     swne = 3  # 시작방향
-    cardinal_points = np.array([[1, 0], [0, -1], [-1, 0], [0, 1]])  #Dex++1, Lavo--1
-    head = np.array([1, 1])
-    snake_size = 1
+    cardinal_points = [[1, 0], [0, -1], [-1, 0], [0, 1]] #Dex++1, Lavo--1 남서북동
+    head = [1, 1]
+    tail = list()
     pre_time = 0
     while True:
-        next_rotate = route_list.pop()
+        next_rotate = route_list.pop(0)
         # 다음 plan 나오는 시간까지 진행방향으로 움직인다
         for second in range(next_rotate[0] - pre_time):
             # 한칸 머리만 내밀어본다
             ans += 1
-            head += cardinal_points[swne]
+            tail.append([head[0], head[1]])
+            head[0] += cardinal_points[swne][0]
+            head[1] += cardinal_points[swne][1]
             # 벽?
-            if head[0] < 1 or head[0] > N or head[1] < 1 or head[1] > N:
+            if head[0] < 1 or head[0] > n or head[1] < 1 or head[1] > n:
                 return ans
             # 몸?
-            elif board[head[0]][head[1]] > 0:
+            elif board[head[0]][head[1]] == 1:
                 return ans
             # Apple
-            elif board[head[0]][head[1]] == -1:
-                snake_size += 1
-                board[head[0]][head[1]] = snake_size
+            elif board[head[0]][head[1]] == 2:
+                board[head[0]][head[1]] = 1
             # 0 and not wall
             else:
-                board = np.where(board > 0, board - 1, board)
-                board[head[0]][head[1]] = snake_size
+                temp1, temp2 = tail.pop(0)
+                board[temp1][temp2] = 0
+                board[head[0]][head[1]] = 1
 
         # Dextro, Lavo
-        if next_rotate[1] == 'D':
+        if next_rotate[1] == "D":
             swne = (swne + 1) % 4
         else:
             swne = (swne - 1) % 4
         pre_time = next_rotate[0]
 
 
-N = int(input())
-K = int(input())
-
-apple = []
-for _ in range(K):
-    a_row, a_col = map(int, input().split())
-    apple.append([a_row, a_col])
-num_rotate = int(input())
-route = []
-for _ in range(num_rotate):
-    sec, direction = input().split()
-    sec = int(sec)
-    route.append([sec, direction])
-
-print(Dummy(N, apple, route))
+print(Dummy())

ea35638ed9b8d5d13d292e46a810b12e518dd472
Yee-Kun Jung <twoketchupplz@gmail.com>
2021-02-27 23:39:57 +0900

    14681 사분면 고르기 풀이

---
+++ CSharp/Baekjoon/14681.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Baekjoon
+{
+    public class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int x = Int16.Parse(Console.ReadLine());
+            int y = Int16.Parse(Console.ReadLine());
+            
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine(1);
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine(2);
+            }
+            else if(x < 0 && y < 0)
+            {
+                Console.WriteLine(3);
+            }
+            else
+            {
+                Console.WriteLine(4);
+            }
+        }
+    }
+}

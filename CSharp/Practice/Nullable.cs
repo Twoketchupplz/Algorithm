b1e49831f4074be1c0a19025163e07b33a6220fa
Yee-Kun Jung <twoketchupplz@gmail.com>
2021-03-02 23:28:08 +0900

    Enum, Nullable 학습

---
+++ CSharp/Practice/Nullable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp.Practice
+{
+    class Nullable
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+
+            Console.WriteLine(a.HasValue); // False
+            Console.WriteLine(a != null); // False
+
+            a = 3;
+
+            Console.WriteLine(a.HasValue); // True
+            Console.WriteLine(a != null); // True
+            Console.WriteLine(a.Value);
+        }
+    }
+}

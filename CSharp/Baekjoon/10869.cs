fb330221b985bcd2cfdb3ada72f4edeebbc2c02a
Yee-Kun Jung <twoketchupplz@gmail.com>
2021-03-05 22:58:47 +0900

    10869 사칙연산 풀이

---
+++ CSharp/Baekjoon/10869.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp.Baekjoon
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int num1, num2;
+            input = Console.ReadLine();
+
+            string[] numbers = input.Split(' ');
+            num1 = Int32.Parse(numbers[0]);
+            num2 = Int32.Parse(numbers[1]);
+            int plus = num1 + num2;
+            int minus = num1 - num2;
+            int multi = num1 * num2;
+            int div = num1 / num2;
+            int remain = num1 % num2;
+            Console.WriteLine(plus);
+            Console.WriteLine(minus);
+            Console.WriteLine(multi);
+            Console.WriteLine(div);
+            Console.WriteLine(remain);
+        }
+    }
+}

f8ce0e1905d9d160666484252b409656ab5a5317
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2021-04-22 18:06:53 +0900

    서울 지하철 2호선 풀이 런타임 에러 해결: 문제 조건상 재귀 횟수는 약 3000번이 최대이다. 하지만 3000을 잡으면 풀리지 않아 4000으로 잡았다.

--- Python/16947_서울_지하철_2호선.py
+++ Python/16947_서울_지하철_2호선.py
@@ -9,98 +9,71 @@ while 사이클만 남을때 까지:
     node[idx]에 연결된 유일한 node v를 찾는다. 윗줄과 함께 O(n^2)
     edge_cnt[v] -= 1
 edge_cnt[idx] 중 val이 0이 아닌 idx가 사이클에 해당하는 node이다.
+
 이후 거리는 BFS로 구한다.
+포인트는 BFS 시작점이 사이클에 해당하는 노드 중 하나라는 점이다.
 """
 
-from collections import deque
+import sys
 
-global edges, N
+sys.setrecursionlimit(4000)
+
+global edges, N, ans, visited
 
 
 def search_cycle():
-    # 각노드의 에지 개수를 담은 리스트 edge_cnt 를 만든다. O(n^2)
+    global ans, visited
+
     linked_edge_ctr = [0 for _ in range(N + 1)]
     for i in range(N + 1):
         cnt = edges[i].count(1)
         linked_edge_ctr[i] = cnt
-    # print(edges)
-    # print(e_cnt)
 
-    # 사이클만 남을 때까지 아래를 반복한다.
+    # 사이클만 남을 때까지 지선의 끝 노드를 자르고 그 노드와 연결된 다른 노드의 에지 개수도 줄여준다.
     v_idx = 0
     ctr = 0
-    # deleted = [0 for _ in range(N+1)]
     while ctr < N:
         v_idx += 1
         if v_idx > N:
             v_idx = 0
         ctr += 1
-        # print("idx", idx)
-        # edge_cnt[idx]의 val이 1인 idx가 지선의 마지막 노드이다. val을 0으로 만든다.
         if linked_edge_ctr[v_idx] == 1:
-            # deleted[v_idx] = 1
             linked_edge_ctr[v_idx] = 0
-            # 마지막 노드와 연결된 살아있는 유일한 node v를 찾는다.
-            for lnk_v_idx in range(1, N+1):
+            for lnk_v_idx in range(1, N + 1):
                 if edges[v_idx][lnk_v_idx] == 1:
-                    # linked_edge_ctr[lnk_v_idx] -= 1
                     linked_edge_ctr[lnk_v_idx] = max(0, linked_edge_ctr[lnk_v_idx] - 1)
             ctr = 0
-            # print("edge counter")
-        # print("idx, e")
-        # print(idx, e_cnt)
 
     # 남은 에지가 0이 아닌 노드가 사이클에 포함되는 노드이다.
-    # print(e_cnt)
     ans = [0 for _ in range(N + 1)]
-    for v_idx in range(N+1):
+    for v_idx in range(N + 1):
         val = linked_edge_ctr[v_idx]
         if val == 0:
             ans[v_idx] = -1
         else:
             ans[v_idx] = 0
-    # print("ans", ans)
-    return ans
+
+    visited = [0 for _ in range(N + 1)]
+
+    return ans.index(0)
 
 
-def BFS(cycle_nodes):
-    ans = cycle_nodes
-    # 노드마다 하나씩 순회한다.
+def BFS(cur):
+    global ans, visited
 
-    for start in range(N+1):
-        # 사이클이면 시작하지 않는다. 0이 사이클임을 의미
-        is_cycle = cycle_nodes[start]
-        if start == 0 or is_cycle == 0:
-            continue
-        # 노드가 지선이면 탐색을 시작한다 사이클을 만날때까지
-        else:
-            # print("--- start node num :", start)
-            queue = deque()
-            distance = [-1 for _ in range(N + 1)]
-            queue.append(start)
-            distance[start] = 0
-            got_ans = False
-            # 하나의 노드로부터 사이클까지의 거리 찾기 시작
-            while queue and not got_ans:
-                cur = queue.popleft()
-                # print("current node:", cur)
-                dist = distance[cur]
-                for lnk_node in range(1, N + 1):
-                    if edges[cur][lnk_node] == 1 and distance[lnk_node] == -1:
-                        if cycle_nodes[lnk_node] == 0:  # 사이클에 닿으면
-                            ans[start] = dist + 1
-                            # print("got ans, distances: ", distance)
-                            got_ans = True
-                            break
-                        distance[lnk_node] = dist + 1
-                        # print("distances:", distance)
-                        queue.append(lnk_node)
-                # print("current queue:", queue)
-        # print("ans of a node", ans)
-    return ans
+    visited[cur] = 1
+    for adj in range(1, N + 1):
+        if visited[adj] == 0:
+            if edges[cur][adj] == 1:
+                if ans[adj] == -1:  # 지선인 경우 거리 정보를 담아준다.
+                    ans[adj] = ans[cur] + 1
+                    BFS(adj)
+                elif ans[adj] == 0:  # 사이클인 경우
+                    BFS(adj)
 
 
-def solution(ans):
+def solution():
+    BFS(search_cycle())
     ans.pop(0)
     for val in ans:
         print(val, end=" ")
@@ -112,4 +85,4 @@ for _ in range(N):
     v1, v2 = map(int, input().split())
     edges[v1][v2] = 1
     edges[v2][v1] = 1
-solution(BFS(search_cycle()))
+solution()

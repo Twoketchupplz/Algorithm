b33f091966a54df5a5ff6a5bfc401ce64bd2340b
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2021-12-16 20:08:21 +0900

    Create 10952_A_plus_B_5.cs
    풀이

---
+++ CS/CS/Baekjoon/10952_A_plus_B_5.cs
@@ -0,0 +1,23 @@
+﻿// https://www.acmicpc.net/problem/10952
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class MainApp {
+    static void Main(string[] args) {
+        int num1, num2;
+        string[] numArr;
+
+        while (true) {
+            numArr = Console.ReadLine().Split(' ');
+            num1 = int.Parse(numArr[0]);
+            num2 = int.Parse(numArr[1]);
+
+            if ((num1 == 0) && (num2 == 0)) break;
+
+            Console.WriteLine(num1 + num2);
+        }
+
+    }
+}

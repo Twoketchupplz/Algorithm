705b7fed5cff71bdc6cd036fc6e8162730b403ec
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2021-12-16 20:08:12 +0900

    Create 10818_최대_최소.cs
    풀이

---
+++ CS/CS/Baekjoon/10818_최대_최소.cs
@@ -0,0 +1,24 @@
+﻿// https://www.acmicpc.net/problem/10818
+
+using System;
+
+class MainApp {
+    static void Main(string[] args) {
+        int N, k;
+        int min = 1000000;
+        int max = -1000000;
+        string[] str;
+
+        N = int.Parse(Console.ReadLine());
+        str = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < N; i++) {
+            k = int.Parse(str[i]);
+            if (min > k) min = k;
+            if (max < k) max = k;
+        }
+
+        Console.WriteLine("{0} {1}", min, max);
+
+    }
+}

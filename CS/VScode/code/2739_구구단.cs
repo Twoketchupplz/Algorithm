919fd22388aad71bc830ab61931e587b972f87cf
Yee-Kun Jung <twoketchupplz@gmail.com>
2021-12-18 15:38:14 +0900

    2739 풀이
    vscode

---
+++ CS/VScode/code/2739_구구단.cs
@@ -0,0 +1,21 @@
+﻿// https://www.acmicpc.net/problem/2739
+
+using System;
+
+class Gugudan
+{
+
+
+    static void Main(string[] args)
+    {
+        string N;
+        int dan;
+        N = Console.ReadLine();
+        dan = int.Parse(N);
+
+        for(int i = 1; i < 10; i++){
+            Console.WriteLine("{0} * {1} = {2}", dan, i, dan*i);
+        }
+    }
+
+}
